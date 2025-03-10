4ffb693c762182810b87feaaeadf34686f3562f6
ssadiq7 <syed.sadiq1990@gmail.com>
2025-03-10 06:58:07 +0530

    Representing Cardinalities in the Models for Database | Feb 15, 2025

--- src/main/java/com/scaler/productservicefeb25/models/Category.java
+++ src/main/java/com/scaler/productservicefeb25/models/Category.java
@@ -1,12 +1,21 @@
 package com.scaler.productservicefeb25.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
 @Entity(name = "categories")
 public class Category extends BaseModel {
     private String name;
+
+    @OneToMany(mappedBy = "category") // mappedBy is used to specify the field in the Product class that owns the relationship
+    // If we do not mention mappedBy then Spring will create an additional table to maintain the relationship
+    // mappedBy can be used only on the non-owning side of the relationship, that is, only on the Many side of the OneToMany relationship
+    private List<Product> products; // This is not a column in the database, it is a field in the entity class
+    // This List will fetch all the products that belong to this category
 }
