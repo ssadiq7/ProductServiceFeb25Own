0385ccbe168bcf6151305b2340b30a2690df0398
Syed Sadiq <syed.sadiq1990@gmail.com>
2025-03-11 19:09:16 +0530

    SelfProductService getAllProducts | Feb 21, 2025

--- src/main/java/com/scaler/productservicefeb25/services/SelfProductService.java
+++ src/main/java/com/scaler/productservicefeb25/services/SelfProductService.java
@@ -77,7 +77,7 @@ public class SelfProductService implements ProductService {
 
     @Override
     public List<Product> getAllProducts() {
-        return List.of();
+        return productRepository.findAll();
     }
 
     @Override
